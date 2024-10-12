cef488f01cab273bf1946d8f56b0a1db70aef734
Kamil <kgruca7747@gmail.com>
2024-10-12 15:52:20 +0200

    Aktualna wersja programu

--- Projekt1/Projekt1/Projekt1.cpp
+++ Projekt1/Projekt1/Projekt1.cpp
@@ -5,9 +5,14 @@ int main()
 {
     replisty lista;
 
-    lista.Lista_Dodanie_na_poczatek('1');
-    lista.Lista_Dodanie_na_poczatek('2');
+    lista.Lista_Dodanie_na_poczatek('1');   // <--- lista w kolejności 1
+    lista.Lista_Dodanie_na_poczatek('2');   // <--- lista w kolejności 21
+    
+    lista.Lista_Dodanie_na_koniec('3');     // <--- lista w kolejności 213
+    lista.Lista_Dodanie_na_koniec('4');     // <--- lista w kolejności 2134
 
     std::cout << "Elementy listy: ";
     lista.Lista_wyswietl();
+
+
 }
