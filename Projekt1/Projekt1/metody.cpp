7d523e87271543955e7120482245a924483ca7b8
Kamil <kgruca7747@gmail.com>
2024-10-12 14:52:57 +0200

    Tworzenie funkcji która wyświetla liste v1

--- Projekt1/Projekt1/metody.cpp
+++ Projekt1/Projekt1/metody.cpp
@@ -1,1 +1,23 @@
-sprawdzanie git merge 
+#include "spis.h"
+#include <iostream>
+
+elisty::elisty(char val) : data(val), prev(0), next(0) {} 
+
+replisty::replisty() : head(0), tail(0), count(0) {} 
+
+replisty::~replisty() {
+}
+
+void replisty::Lista_Dodanie_na_poczatek(char v) {
+    elisty* p = new elisty(v);
+    p->next = head;
+    head = p; 
+    if (count == 0) {
+        tail = p;
+    }
+    else {
+        p->next->prev = p;
+    }
+    count++;
+}
+
