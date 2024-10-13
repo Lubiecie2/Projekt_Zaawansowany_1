d5697d1f6a880af40b489d068aa63c6c329780d3
Kamil <kgruca7747@gmail.com>
2024-10-13 12:20:48 +0200

    Wersja Beta programu

--- Projekt1/Projekt1/Projekt1.cpp
+++ Projekt1/Projekt1/Projekt1.cpp
@@ -39,7 +39,7 @@ int main()
     lista.Lista_Usuwanie_pierwszego_elementu();
     lista.Lista_wyswietl();
 
-    std::cout << "Lista po usunieciu ostatniego elementu: ";
+    std::cout << "Lista. po usunieciu ostatniego elementu: ";
     lista.Lista_usuwanie_ostatniego_elementu();
     lista.Lista_wyswietl();
 
